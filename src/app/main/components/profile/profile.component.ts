32a443a8c34ac21fe31fe7cf4b7ab12f80ef31cb
Paul Schweng <paul.schweng@siemens.com>
2023-03-08 20:28:03 +0100

    added logout button

--- src/app/main/components/profile/profile.component.ts
+++ src/app/main/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../../../services/authentication.service";
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private readonly authService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    console.log('here')
+    this.authService.logout()
+      .then();
+  }
 }

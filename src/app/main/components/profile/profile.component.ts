1e7779621f15c5dedea14a29916e9f927537fc78
paul <paul.schweng@siemens.com>
2023-04-05 13:21:55 +0200

    added profile avatar and name

--- src/app/main/components/profile/profile.component.ts
+++ src/app/main/components/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from "../../../services/authentication.service";
+import {IAmService} from "../../../services/i-am.service";
 
 @Component({
   selector: 'app-profile',
@@ -8,7 +9,8 @@ import {AuthenticationService} from "../../../services/authentication.service";
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private readonly authService: AuthenticationService) { }
+  constructor(private readonly authService: AuthenticationService,
+              public readonly iAmService: IAmService) { }
 
   ngOnInit(): void {
   }
